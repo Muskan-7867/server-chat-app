aff500af667ad31648c235399caf9bc6372b9d81
muskan <muskanloach984@gmail.com>
2025-02-10 22:18:11 +0530

    fix issues

--- app.js
+++ app.js
@@ -1,51 +1,65 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
-import  { createServer } from "http";
-import { Server } from 'socket.io';
-// import dotenv from 'dotenv';
+import { createServer } from "http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+dotenv.config();
 
-// dotenv.config(); 
 
 const port = 3000;
 const app = express();
-
-//http server
 const server = createServer(app);
 
-//websocket server using socket.io
-const io = new Server(server, {
-  cors: {
-    origin: "https://client-chat-app-chi.vercel.app",  
+const allowedOrigins = [
+  process.env.VITE_FRONTEND_URL || "http://localhost:5173",
+];
+
+
+app.use(
+  cors({
+    origin: allowedOrigins,
     methods: ["GET", "POST"],
     credentials: true,
-  }},
+  })
 );
 
-app.use(cors({
-  origin: "https://client-chat-app-chi.vercel.app" ,
-  methods:["GET" , "POST"],
-  credentials: true,
-}));
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+    credentials: true,
+    transports: ["websocket", "polling"], 
+  },
+});
 
 app.get("/", (req, res) => {
-  res.send(" Hello world")
+  res.send("Hello world");
 });
 
-//listen for client connection  Handles New User Connections
-io.on("connection" , (socket) =>{
-  console.log("user connected", socket.id);  //socket show connection with specific user
+io.on("connection", (socket) => {
+  console.log("new User connected:", socket.id);
+  //every user have new unique socket id
+   io.emit('your-socket-id',socket.id)
 
-//listen for incoming msg that client send
-socket.on("send_message", (data) => {
-  io.emit("receive_message" , data)  //broadcast msg to all users
-})
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data);
+    io.emit("after-send-sockerid", socket.id)
+  });
 
-//listen for user disconnect
-socket.on("disconnect" , () =>{
-  console.log("user disconnected", socket.id);
-})
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
 });
 
-server.listen(port , () => {
-  console.log(`Chat app server is running on ${port}`);
-})
+// Ensure the server starts properly
+const startServer = async () => {
+  try {
+    server.listen(port, () => {
+      console.log(`Chat app server is running on ${port}`);
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+};
+
+startServer();

f2f39cd6f6dd06dbd4d400b8a521b3c772b962cf
muskan <muskanloach984@gmail.com>
2025-02-14 16:45:01 +0530

    fix issues

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
+// Backend: server.js
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv"
-dotenv.config();
 import { createServer } from "http";
 import { Server } from "socket.io";
+import dotenv from "dotenv";
+dotenv.config();
 
 const port = 3000;
-
 const app = express();
-//create an http server
-const server = createServer(app)  
+const server = createServer(app);
 
-const allowedOrigins = [process.env.VITE_FRONTEND_URL || "http://localhost:5173"];
+const allowedOrigins = [  "https://client-chat-nine.vercel.app"];
 
 app.use(
   cors({
@@ -22,40 +21,36 @@ app.use(
 );
 
 const io = new Server(server, {
-  cors:{
+  cors: {
     origin: allowedOrigins,
     methods: ["GET", "POST"],
     credentials: true,
-  }
+    transports: ["websocket", "polling"],
+  },
 });
 
-
-app.get("/", (req,res) =>{
- res.send("hello world");
-})
-
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 io.on("connection", (socket) => {
-  console.log("user connected" , socket.id)
+  console.log("New User connected:", socket.id);
+  
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room: ${room}`);
+    io.to(room).emit("user_joined", { userId: socket.id, room });
+  });
 
-
-socket.on("join_room", (room) => {
-  socket.join(room);
-  console.log(`User ${socket.id} joined room: ${room}`);
-  io.to(room).emit("user_joined", { userId: socket.id, room })
-});
-
-socket.on("send_message", ({message, room, username}) => {
-  io.to(room).emit("receive_message", { message, sender: socket.id, username });
+  socket.on("send_message", ({ message, room, username }) => {
+    io.to(room).emit("receive_message", { message, sender: socket.id, username });
+  });
 
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
   });
 });
 
-
-});
-
 server.listen(port, () => {
-  console.log(`chat app server is running on ${port}`)
-})
+  console.log(`Chat app server is running on ${port}`);
+});
